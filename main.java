91693ab9b4dad4ef9dacd2bdd27b5470a4d5e0f8
EA - Érick Henrique Medeiros De Lima <erick_lima@acad.unasp.edu.br>
2026-05-21 20:28:12 -0300

    adicionei o o token e as variaveis

--- main.java
+++ main.java
@@ -3,3 +3,16 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    private static final String TOKEN = "TOKEN";
+
+    private static final String API_URL =
+            "https://api.telegram.org/bot" + TOKEN + "/";
+
+    private static int updateId = 0;
